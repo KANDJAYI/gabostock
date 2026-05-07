01b099c56279cbcf0bf71d54bc3c4b17f7dddbb2
KANDJAYI <kandjayiodoumachristroy@gmail.com>
2026-05-07 21:24:25 +0100

    correct erreur

--- gabostock/app/(admin)/admin/public-site/page.tsx
+++ gabostock/app/(admin)/admin/public-site/page.tsx
@@ -1,4 +1,4 @@
-import { AdminPublicSiteScreen } from "@/components/admin/admin-public-site-screen";
+import { AdminPublicSiteScreen } from "../../../../components/admin/admin-public-site-screen";
 
 export default function AdminPublicSitePage() {
   return <AdminPublicSiteScreen />;
